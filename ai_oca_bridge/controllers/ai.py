81c3682020b4c364829ecef5278d19b4f62de18c
gerard <gerard@domatix.cloud>
2026-06-22 09:21:50 +0000

    [19.0][MIG] ai_oca_bridge: Migración a 19.0

---
+++ ai_oca_bridge/controllers/ai.py
@@ -0,0 +1,51 @@
+# Copyright 2025 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+import json
+
+from odoo import fields, http
+from odoo.http import request
+from odoo.tools import consteq
+
+
+class AIController(http.Controller):
+    @http.route(
+        [
+            "/ai/response/<int:execution_id>/<string:token>",
+        ],
+        type="http",
+        auth="public",
+        cors="*",
+        csrf=False,
+    )
+    def ai_process_response(self, execution_id, token):
+        execution = request.env["ai.bridge.execution"].sudo().browse(execution_id)
+        if not execution.exists():
+            return request.make_response(
+                request.env._("Execution not found."), status=404
+            )
+        if not consteq(execution._generate_token(), token):
+            return request.make_response(
+                request.env._("Token is not allowed for this execution."), status=404
+            )
+        if (
+            not execution.expiration_date
+            or execution.expiration_date < fields.Datetime.now()
+        ):
+            return request.make_response(
+                request.env._("Execution is expired."), status=404
+            )
+        return request.make_response(
+            json.dumps(
+                execution._process_response(
+                    json.loads(
+                        request.httprequest.get_data().decode(
+                            getattr(request.httprequest, "charset", None) or "utf-8"
+                        )
+                    )
+                )
+            ),
+            headers=[
+                ("Content-Type", "application/json"),
+            ],
+        )
